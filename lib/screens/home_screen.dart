e2d056bebabe3516c6d133368f5630ab07d5e9c7
logann99 <ahmed.201317499@gmail.com>
2023-04-12 01:04:16 +0200

    24. Implement the sale widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:card_swiper/card_swiper.dart';
 
 import '../provider/utils.dart';
+import '../widgets/on_sale_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -23,23 +24,29 @@ class _HomeScreenState extends State<HomeScreen> {
     final themeState = utils.getTheme;
     Size size = utils.getScreenSize;
     return Scaffold(
-      body: SizedBox(
-        height: size.height * 0.33,
-        child: Swiper(
-          itemBuilder: (BuildContext context, int index) {
-            return Image.asset(
-              _offerImagesList[index],
-              fit: BoxFit.fill,
-            );
-          },
-          autoplay: true,
-          itemCount: _offerImagesList.length,
-          pagination: const SwiperPagination(
-              alignment: Alignment.bottomCenter,
-              builder: DotSwiperPaginationBuilder(
-                  color: Colors.white, activeColor: Colors.red)),
-          // control: const SwiperControl(color: Colors.black),
-        ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: size.height * 0.33,
+            child: Swiper(
+              itemBuilder: (BuildContext context, int index) {
+                return Image.asset(
+                  _offerImagesList[index],
+                  fit: BoxFit.fill,
+                );
+              },
+              autoplay: true,
+              itemCount: _offerImagesList.length,
+              pagination: const SwiperPagination(
+                  alignment: Alignment.bottomCenter,
+                  builder: DotSwiperPaginationBuilder(
+                      color: Colors.white, activeColor: Colors.red)),
+
+              // control: const SwiperControl(color: Colors.black),
+            ),
+          ),
+          const OnSaleWidget(),
+        ],
       ),
     );
   }
---
+++ lib/widgets/on_sale_widget.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:grocery_app/provider/utils.dart';
+import 'package:grocery_app/widgets/price_widget.dart';
+import 'package:grocery_app/widgets/text_widget.dart';
+
+class OnSaleWidget extends StatefulWidget {
+  const OnSaleWidget({super.key});
+
+  @override
+  State<OnSaleWidget> createState() => _OnSaleWidgetState();
+}
+
+class _OnSaleWidgetState extends State<OnSaleWidget> {
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Utils(context).color;
+    final Utils utils = Utils(context);
+    final themeState = utils.getTheme;
+    Size size = utils.getScreenSize;
+    return Material(
+      color: Theme.of(context).cardColor.withOpacity(
+            0.9,
+          ),
+      borderRadius: BorderRadius.circular(
+        12,
+      ),
+      child: InkWell(
+        borderRadius: BorderRadius.circular(
+          12,
+        ),
+        onTap: () {},
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.network(
+                    'https://i.ibb.co/F0s3FHQ/Apricots.png',
+                    // width: size.width*0.22,
+                    height: size.width * 0.22,
+                    fit: BoxFit.fill,
+                  ),
+                  Column(
+                    children: [
+                      TextWidget(
+                        textTitle: '1KG',
+                        textColor: color,
+                        textSize: 22,
+                        isTitle: true,
+                      ),
+                      const SizedBox(
+                        height: 6,
+                      ),
+                      Row(
+                        children: [
+                          GestureDetector(
+                            onTap: () {},
+                            child: Icon(
+                              IconlyLight.bag2,
+                              size: 22,
+                              color: color,
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              print('print heart button is pressed');
+                            },
+                            child: Icon(
+                              IconlyLight.heart,
+                              size: 22,
+                              color: color,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              const PriceWidget(),
+              const SizedBox(height: 5),
+              TextWidget(
+                textTitle: 'Product title',
+                textColor: color,
+                textSize: 16,
+                isTitle: true,
+              ),
+              const SizedBox(height: 5),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
