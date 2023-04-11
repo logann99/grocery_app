eb0b13b17c47b3d75e220e16241305803c743038
logann99 <ahmed.201317499@gmail.com>
2023-04-11 19:58:32 +0200

    21. Implement the category design

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/widgets/categories_widget.dart';
+
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    
     return const Scaffold(
       body: Center(
-        child: Text(
-          'Categories Screen',
-        ),
+        child: CategoriesWidget(),
       ),
     );
   }
--- lib/widgets/categories_widget.dart
+++ lib/widgets/categories_widget.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/widgets/text_widget.dart';
+import 'package:provider/provider.dart';
+import '../provider/dark_theme_provider.dart';
 
 class CategoriesWidget extends StatelessWidget {
   const CategoriesWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    // Size size = MediaQuery.of(context).size;
+    double screenWidth = MediaQuery.of(context).size.width;
+    final themeState = Provider.of<DarkThemeProvider>(context);
+    final Color color = themeState.getDarkTheme ? Colors.white : Colors.black;
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.red.withOpacity(
+            0.1,
+          ),
+          borderRadius: BorderRadius.circular(
+            16,
+          ),
+          border: Border.all(
+            color: Colors.red.withOpacity(
+              0.7,
+            ),
+            width: 2,
+          ),
+        ),
+        child: Column(
+          children: [
+            Container(
+              height: screenWidth * 0.3,
+              width: screenWidth * 0.3,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/images/cat/veg.png',
+                  ),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+
+            // Category name
+            TextWidget(
+              textTitle: 'Category name',
+              textColor: color,
+              textSize: 20,
+              isTitle: true,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
